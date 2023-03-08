c44b30623401c790c9d67965c6f4b0501f9bca47
skunk00393 <angel_avramovski@outlook.com>
2023-03-08 20:02:54 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Promena napravena
         </a>
       </header>
     </div>
